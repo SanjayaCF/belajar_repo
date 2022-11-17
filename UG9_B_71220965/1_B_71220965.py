c8e374d4b787fbcd6ce713eb0b7645139806f051
SanjayaCF <sanjaya.cahyadi@ti.ukdw.ac.id>
2022-11-17 13:20:33 +0700

    mengumpulkan file ug9

---
+++ UG9_B_71220965/1_B_71220965.py
@@ -0,0 +1,2 @@
+diameter = int(input('Masukan Diameter (Meter): '))
+print(f'Jarak yang harus ditempuh : {int(3.14*diameter)} Meter')
---
+++ UG9_B_71220965/2_B_71220965.py
@@ -0,0 +1,2 @@
+n = int(input('Masukan Angka Yang Ingin Dipangkat 7\t:'))
+print(f'Hasil dari {n}^7 adalah \t: {n**7}')
