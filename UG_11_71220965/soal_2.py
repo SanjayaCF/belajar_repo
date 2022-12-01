5251b65e82750216cc2ccba1920b675d26ddf60c
SanjayaCF <sanjaya.cahyadi@ti.ukdw.ac.id>
2022-12-01 12:54:58 +0700

    tambah komen soal 2 ug11

--- UG_11_71220965/soal_2.py
+++ UG_11_71220965/soal_2.py
@@ -1,5 +1,5 @@
 def funPalindrome(s):
-    s = s.lower()
+    s = s.lower()#case insensitive
     ispalindrome = 'Yes' if s == s[::-1] else 'No'
     return f'{ispalindrome}\nJika Dibalik: {s[::-1]}'
 
